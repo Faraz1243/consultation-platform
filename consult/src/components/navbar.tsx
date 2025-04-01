88581925ad07dda23b9bbd53514534747fd762b2
Faraz1243 <farazaliasif786@gmail.com>
2025-04-01 11:00:00 +0500

    UI UPDATES NAVBAR

--- consult/src/components/navbar.tsx
+++ consult/src/components/navbar.tsx
@@ -1,14 +1,14 @@
-import React, { Component } from 'react'
-import Logo from '../assets/img/logo1.png'
-import LogoSmall from '../assets/img/logoSmall.png'
-import { Link, NavLink } from 'react-router-dom'
-import { useState } from 'react'
+import React, { Component } from "react";
+import Logo from "../assets/img/logo1.png";
+import LogoSmall from "../assets/img/logoSmall.png";
+import { Link, NavLink } from "react-router-dom";
+import { useState } from "react";
 
-
+// NAVBAR UPDATE
 export class navbar extends Component {
   render() {
     return (
-        <header className="header header-one">
+      <header className="header header-one">
         <div className="container">
           <nav className="navbar navbar-expand-lg header-nav">
             <div className="navbar-header">
@@ -23,11 +23,7 @@ export class navbar extends Component {
                 <img src={Logo} className="img-fluid" alt="Logo" />
               </Link>
               <Link to="/" className="navbar-brand logo-small">
-                <img
-                  src={LogoSmall}
-                  className="img-fluid"
-                  alt="Logo"
-                />
+                <img src={LogoSmall} className="img-fluid" alt="Logo" />
               </Link>
             </div>
             <div className="main-menu-wrapper">
@@ -35,39 +31,64 @@ export class navbar extends Component {
                 <Link to="/" className="menu-logo">
                   <img src={Logo} className="img-fluid" alt="Logo" />
                 </Link>
-                <a id="menu_close" className="menu-close" href="javascript:void(0);">
+                <a
+                  id="menu_close"
+                  className="menu-close"
+                  href="javascript:void(0);"
+                >
                   {" "}
                   <i className="fas fa-times" />
                 </a>
               </div>
-              {this.props.noTabs ? null : 
-              <ul className="main-nav">
-                <li className=" megamenu ">
-                  <NavLink to="/" className={(isActive)=>isActive? "active":""}>Home</NavLink>
-                </li>
-                <li className=" megamenu ">
-                  <NavLink to="/user/dashboard" className={(isActive)=>isActive? "active":""}>Dashboard</NavLink>
-                </li>
-                <li className="">
-                  <NavLink to="/consultants" className={(isActive)=>isActive? "active":""}>Consultants</NavLink>
-                </li>
-                {/* <li className="has-submenu">
+              {this.props.noTabs ? null : (
+                <ul className="main-nav">
+                  <li className=" megamenu ">
+                    <NavLink
+                      to="/"
+                      className={(isActive) => (isActive ? "active" : "")}
+                    >
+                      Home
+                    </NavLink>
+                  </li>
+                  <li className=" megamenu ">
+                    <NavLink
+                      to="/user/dashboard"
+                      className={(isActive) => (isActive ? "active" : "")}
+                    >
+                      Dashboard
+                    </NavLink>
+                  </li>
+                  <li className="">
+                    <NavLink
+                      to="/consultants"
+                      className={(isActive) => (isActive ? "active" : "")}
+                    >
+                      Consultants
+                    </NavLink>
+                  </li>
+                  {/* <li className="has-submenu">
                   <NavLink to="/partners" className={(isActive)=>isActive? "active":""}>
                     Partners <i className="fas fa-chevron-down" />
                   </NavLink>
                 </li> */}
-                <li className="nav-item">
-                  <NavLink to="/ai-agents" className={(isActive)=>isActive? "active":""}>
-                    AI Agents
-                  </NavLink>
-                </li>
-                <li className="nav-item">
-                  <NavLink className={(isActive)=>isActive? "active":""} to="/about">
-                    About
-                  </NavLink>
-                </li>
-              </ul>
-              }
+                  <li className="nav-item">
+                    <NavLink
+                      to="/ai-agents"
+                      className={(isActive) => (isActive ? "active" : "")}
+                    >
+                      AI Agents
+                    </NavLink>
+                  </li>
+                  <li className="nav-item">
+                    <NavLink
+                      className={(isActive) => (isActive ? "active" : "")}
+                      to="/about"
+                    >
+                      About
+                    </NavLink>
+                  </li>
+                </ul>
+              )}
             </div>
             <ul className="nav header-navbar-rht">
               <li className="nav-item">
@@ -80,9 +101,8 @@ export class navbar extends Component {
           </nav>
         </div>
       </header>
-      
-    )
+    );
   }
 }
 
-export default navbar
+export default navbar;
